2b784a29137b2d947e71df65f2dd59e86f947be0
snorri.sturluson <snorri.sturluson@ccpgames.com>
2016-05-24 11:29:21 +0000

    Changed to interactive.

--- TestVirtualStore.cpp
+++ TestVirtualStore.cpp
@@ -3,36 +3,72 @@
 #include <fstream>
 #include <string>
 
-const char* FILENAME = "c:\\ProgramData\\TestVirtualStore\\testvirtualstore.txt";
+const char* FOLDER1 = "C:\\Program Files\\TestVirtualStore";
+const char* FOLDER2 = "C:\\ProgramData\\TestVirtualStore";
+const char* FILENAME = "myfile.txt";
 
 int _tmain( int argc, _TCHAR* argv[] )
 {
-	std::string prevContents;
-	int lineCount = 0;
+	std::cout << "This program illustrates issues with VirtualStore" << std::endl;
+	std::cout << "1 - Program Files folder (ensure " << FOLDER1 << " exists)" << std::endl;
+	std::cout << "2 - ProgramData folder (ensure " << FOLDER2 << " exists)" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Please select: ";
+	std::string selection;
+	std::getline( std::cin, selection );
+	
+	std::string filename;
+	if( selection == "1" )
 	{
-		std::ifstream input( FILENAME );
-		while( input.good() )
-		{
-			std::string line;
-			std::getline( input, line );
-			prevContents += line + '\n';
-			lineCount++;
-		}
+		filename = FOLDER1;
 	}
-
-	std::ofstream output;
-	output.open( FILENAME, std::ofstream::out | std::ofstream::trunc );
-	if( output.good() )
+	else if( selection == "2" )
 	{
-		output << prevContents;
-		output << "This is line number " << lineCount + 1;
+		filename = FOLDER2;
 	}
 	else
 	{
-		std::cout << "Couldn't open file " << FILENAME << std::endl;
+		return 0;
+	}
+
+	filename += "\\";
+	filename += FILENAME;
+
+	std::ifstream input( filename );
+	if( input.good() )
+	{
+		std::string line;
+		std::getline( input, line );
+
+		std::cout << filename << " was found and contains:" << std::endl;
+		std::cout << line << std::endl;
+
 		std::string dummy;
 		std::getline( std::cin, dummy );
+
+		return 0;
 	}
+
+	std::cout << filename << " was not found." << std::endl;
+
+	std::ofstream output;
+	output.open( filename, std::ofstream::out | std::ofstream::trunc );
+	if( output.good() )
+	{
+		std::cout << "Please enter some text:" << std::endl;
+
+		std::string line;
+		std::getline( std::cin, line );
+
+		output << line << std::endl;
+	}
+	else
+	{
+		std::cout << "Couldn't create file " << filename << std::endl;
+	}
+
+	std::string dummy;
+	std::getline( std::cin, dummy );
 
 	return 0;
 }
